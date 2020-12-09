5e808e94d1f21d33eed483f1c174dbc141e9732b
Melody Monarch <oliva.sam.so@gmail.com>
2020-12-09 14:21:38 -0600

    began on-click functions for home and portfolio

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,14 @@
 console.log("hi");
+
+$("#briefcase").on("click", () => {
+    console.log("hi")
+    $(".fa-briefcase").css("color", " #ffffff" )
+    $("#homeBtn").css("color", "var(--navy)" )
+    $("#mainArea").html("")
+})
+
+$("#homeBtn").on("click", () => {
+    console.log("hi")
+    $(".fa-briefcase").css("color", "var(--navy)" )
+    $("#homeBtn").css({"color": "#ffffff", "display" : "block"})
+})
